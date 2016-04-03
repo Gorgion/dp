325ccf9c7f07bd25864092d68f6b6e08652553c8
tomas.svoboda <tomas.svoboda@ibacz.eu>
2016-04-03 12:55:31 +0200

    clean

--- portlet-application/core/target/test-classes/sql/dummy.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM dummy;
-
-INSERT INTO dummy (id, dummy_name, dummy_email) VALUES (-1, 'Bedřich', 'bedrich@example.com');
--- portlet-application/portlet/target/dp-portlet-application-1.0-SNAPSHOT/js/portlets/common/portlet-reusable.js
+++
@@ -1,3 +0,0 @@
-/**
-This script can be used to store code used in more then one portlet but the code is not a component
- */
